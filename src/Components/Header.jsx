fbfc0d500ff74cea7bd46c5fa7b85da41b886873
nikhilsharma9548 <ns630403@gmail.com>
2025-04-23 17:33:37 +0530

    change

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
             src={images}
             alt="logo"
             className="w-20 h-20 items-center max-md:h-12 max-md:w-12 max-md:mx-0 rounded-full shadow-xl mx-5 object-cover"
-          /> <h1 className=" text-2xl max-md:text-[17px]">PORTFOLIO</h1>
+          /> <h1 className=" text-2xl max-md:text-[20px]">PORTFOLIO</h1>
       </div>
       <div className="flex text-xl w-[25%] max-md:hidden  items-center justify-center gap-8 shadow-lg rounded-full h-20 mt-2">
           <button >Home</button>
@@ -19,8 +19,8 @@ const Header = () => {
           <button>Projects</button>
           <button>Skills</button>
       </div>
-      <div className="items-center flex justify-center max-md:hidden">
-        <h1 className="p-4  mx-6 bg-white text-[18px] rounded-full hover:cursor-pointer flex gap-3  "><FaRegMessage style={{ fontSize: "1.4em" }} />CONTACT ME</h1>
+      <div className="items-center  flex justify-center max-md:hidden">
+        <h1 className="p-4  mx-6 bg-white text-[18px] rounded-full hover:cursor-pointer flex gap-2  "><FaRegMessage style={{ fontSize: "1.2em"}} />CONTACT ME</h1>
       </div>
 
       
