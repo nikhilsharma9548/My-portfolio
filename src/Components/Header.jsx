fedd2fe7a56d436f809e2e78340c7c684c5e4891
nikhilsharma9548 <ns630403@gmail.com>
2025-04-23 16:45:38 +0530

    uppp

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
             src={images}
             alt="logo"
             className="w-24 h-24 items-center rounded-full shadow-xl mx-5 object-cover"
-          /> <h1 className=" text-2xl max-md:center">PORTFOLIO</h1>
+          /> <h1 className=" text-2xl max-md:text-[12px]">PORTFOLIO</h1>
       </div>
       <div className="flex text-2xl w-[25%] max-md:hidden  items-center justify-center gap-10 shadow-lg rounded-full h-24 mt-2">
           <button >Home</button>
