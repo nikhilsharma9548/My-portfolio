2a4e7137325a169a76a5dc5bf7f9721a2f44b116
nikhilsharma9548 <ns630403@gmail.com>
2025-04-26 15:04:19 +0530

    committed

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,19 +3,19 @@ import image4 from "../images/img2.jpg";
 
 const About = () => {
   return ( 
-    <section id='About' className="h-screen pt-40  bg-gradient-to-r from-gray-200 to-gray-700">
+    <section id='About' className="h-auto  bg-gradient-to-r from-gray-200 to-gray-700">
       <h1 className='flex items-center justify-center text-5xl font-serif font-bold'>ABOUT-ME</h1>
 
-      <div className='flex  justify-center w-full items-center gap-32 relative right-32'>
+      <div className='flex max-md:flex-col justify-center w-full items-center gap-32 relative right-32'>
                     <div className="flex justify-center md:justify-end w-full md:w-1/2">
                        <img
                       src={image4}
                       alt="Nikhil"
-                        className="w-64 h-80 md:w-96 md:h-[470px] rounded-3xl shadow-lg border-4 mt-20 border-gray-400 object-cover"
+                        className="w-72 h-96 max-md:relative left-32 md:w-96 md:h-[470px] rounded-3xl shadow-lg border-4 mt-20 border-gray-400 object-cover"
                        />
                    </div>
 
-                   <div className='w-full md:w-[80%]  justify-center md:justify-start items-center'>
+                   <div className='w-full md:w-[80%] max-md:flex-col  justify-center md:justify-start items-center'>
                     <h1 className=" w-[80%]  text-2xl text-balance leading-relaxed">I am a <span className='font-bold'>Frontend Web Developer</span>. I build projects using <span className='font-bold'> HTML, CSS, JavaScript, React, and TailwindCSS</span>. I am always eager to learn new technologies and improve my skills.</h1>
 
                     <div className='flex gap-10 relative top-20'>
