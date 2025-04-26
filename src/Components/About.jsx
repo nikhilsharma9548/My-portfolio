420dda0cdd060e66ef40f5f21a3c4143983c972e
nikhilsharma9548 <ns630403@gmail.com>
2025-04-26 18:37:55 +0530

    hello

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,15 +7,15 @@ import image4 from "../images/img2.jpg";
 
 const About = () => {
   return ( 
-    <section id='About' className="h-full pt-40 overflow-hidden bg-gradient-to-r from-gray-200 to-gray-700">
-      <h1 className='flex items-center justify-center text-5xl font-serif font-bold'>ABOUT-ME</h1>
+    <section id='About' className="h-screen pt-40 overflow-hidden bg-gradient-to-r from-gray-200 to-gray-700">
+      <h1 className='flex items-center justify-center text-4xl font-serif font-bold'>ABOUT-ME</h1>
 
       <div className='flex max-md:flex-col  justify-center w-full items-center gap-32 relative right-32'>
                     <div className="flex justify-center md:justify-end w-full  md:w-1/2">
                        <img
                       src={image4}
                       alt="Nikhil"
-                        className="w-72 h-96 max-md:relative left-32 md:w-96 md:h-[470px] rounded-3xl shadow-lg border-4 mt-20 border-gray-400 object-cover hover:scale-105 transition duration-300  cursor-pointer"
+                        className="w-72 h-96 max-md:relative left-32 md:w-96 md:h-[500px] rounded-3xl shadow-lg  mt-20 border-gray-400 object-cover hover:scale-105 transition duration-500 translate-y-0  cursor-pointer"
                        />
                    </div>
 
@@ -25,22 +25,22 @@ const About = () => {
                     <div className='flex max-md:flex-col text-gray-950 gap-10 relative md:top-20 
                     max-md:left-[11.5rem] max-md:bottom-10 max-md:text-balance  '>
 
-                      <div className='h-52 w-72 bg-transparent  border-2 rounded-lg transition-all duration-500 hover:scale-105  '>
+                      <div className='h-52 w-72 bg-transparent  border-2 rounded-lg transition-all duration-500 hover:scale-105 hover:border-b-8 hover:border-r-8 hover:border-red-600'>
                         <p className='text-5xl px-6 pt-6'><FaCode /></p>
                         <p className='px-5 pt-2 text-xl'>Languages</p>
                         <p className='text-xl p-3 mx-3'>HTML, CSS, JavaScript, ReactJS, TailwindCSS</p>
                       </div>
 
-                      <div className='h-52 w-72 bg-transparent border-2 rounded-lg transition-all duration-500 hover:scale-105  '>
+                      <div className='h-52 w-72 bg-transparent border-2 rounded-lg transition-all duration-500 hover:scale-105 hover:border-b-8 hover:border-r-8 hover:border-red-600' >
                         <p className='text-5xl px-6 pt-6'><PiGraduationCapFill /></p>
                         <h1 className='px-5 pt-2 text-xl'>Education</h1>
-                        <h1 className='text-xl p-3 mx-3 opacity-1'>Bachelor of Computer Applications(BCA)[Present]</h1>
+                        <h1 className='text-xl p-2 mx-3 opacity-1'>Bachelor of Computer Applications(BCA)[Present]</h1>
                       </div>
 
-                      <div className='h-52 w-72 bg-transparent  border-2 rounded-lg transition-all duration-500 hover:scale-105 '>
+                      <div className='h-52 w-72 bg-transparent  border-2 rounded-lg transition-all duration-500 hover:scale-105 hover:border-b-8 hover:border-r-8 hover:border-red-600'>
                         <p className='text-5xl px-6 pt-6'><VscFileSubmodule /></p>
                         <h1 className='px-5 pt-2 text-xl'>Projects</h1>
-                        <h1 className='text-xl p-4 mx-3 opacity-'>Built more than 5 projects</h1>
+                        <h1 className='text-xl p-3 mx-3 opacity-'>Built more than 5 projects</h1>
                       </div>
 
                     </div>
