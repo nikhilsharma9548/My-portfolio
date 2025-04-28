c064090fd86e54d3564c356edeeccd6f7ec764cb
nikhilsharma9548 <ns630403@gmail.com>
2025-04-28 19:48:35 +0530

    done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <section className='h-24 bg-gray-900 w-full  text-white text-xl flex justify-center items-center'>
-          <h1>© 2025 Nikhil Sharma | All Rights Reserved</h1>
+          <h1 className='max-md:text-lg'>© 2025 Nikhil Sharma | All Rights Reserved</h1>
     </section>
   )
 }
