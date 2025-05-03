b463dcfcf181931ab9ef277f51462d97b758d08d
nikhilsharma9548 <ns630403@gmail.com>
2025-05-03 08:49:08 +0530

    comiite

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
       
 
     return(
-       <section  id="Contact" className=" md:pt-14 bg-gradient-to-r from-gray-200 to-gray-700 h-full overflow-hidden  flex-col justify-center sm:pt-0">
+       <section  id="Contact" className=" md:pt-14 pt-5 bg-gradient-to-r from-gray-200 to-gray-700 h-full overflow-hidden  flex-col justify-center sm:pt-0">
         <h1 className='flex items-center justify-center text-4xl font-bold'>Contact-Me</h1>
         <p className='text-center text-xl md:text-2xl mt-20'>Please Fill out the form below to discuss anywork opportunities.</p>
         <div className='mt-10 '>
