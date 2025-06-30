3d1e9feb9852ebb73290bea171c0178cc5cefa5b
nikhilsharma9548 <ns630403@gmail.com>
2025-07-01 02:09:10 +0530

    push

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,6 +3,8 @@ import WeatherApp from "../images/WeatherApp.png";
 import LMS from "../images/Padho-Likho.png";
 import { IoLogoGithub } from "react-icons/io";
 import { LuLink } from "react-icons/lu";
+import chatApp from "../images/chatApp.png"
+import { image } from "framer-motion/client";
 const Projects = () => {
     const projectList = [
       {
@@ -21,16 +23,22 @@ const Projects = () => {
         image:WeatherApp,
       
       },
+      {
+        title:"Chat App",
+        description:"A Chat App bulid to using react js ,tailwind CSS, gemini API ",
+        demo: "https://chat-app-one-alpha-88.vercel.app/",
+        image:chatApp
+      }
     ];
   
     return (
-      <section id="projects"  className="py-14 h-auto lg:h-screen bg-gradient-to-r from-gray-200 to-gray-700 text-gray-800">
+      <section id="projects"  className="py-14 bg-gradient-to-r from-gray-200 to-gray-700 text-gray-800">
         <div className="text-center mb-10">
           <h2 className="md:text-4xl text-3xl font-semibold">My Projects</h2>
           <p className="md:text-lg text-base mt-2">Some cool stuff I’ve built recently 👇</p>
         </div>
 
-        <div className="max-w-4xl max-md:w-[80%]  mx-auto md:px-4 grid md:grid-cols-2 gap-8">
+        <div className="max-w-7xl max-md:w-[80%]  mx-auto md:px-4 grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projectList.map((project, index) => (
             <div key={index} className="bg-white p-3 rounded-xl  shadow-md hover:shadow-xl transition duration-300">
               {project.image && (
