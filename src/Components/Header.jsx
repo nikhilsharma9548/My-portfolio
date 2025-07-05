391220b82547dd227c5ab38e962a7970264b01af
nikhilsharma9548 <ns630403@gmail.com>
2025-07-05 18:47:18 +0530

    push

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,8 @@ import { HiOutlineBars2 } from "react-icons/hi2";
 import { GiCrossedBones } from "react-icons/gi";
 import { Link } from "react-scroll";
 import images from "../images/img3.png";
-import { menu, p } from "framer-motion/client";
+import { motion, AnimatePresence } from "framer-motion";
+import { div } from "framer-motion/client";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -22,18 +23,18 @@ const Header = () => {
 
   return (
     <>
-    <section id="header"  className={` fixed top-0 left-0 w-full  bg-gradient-to-r from-gray-200 to-gray-700 text-black font-serif shadow-lg z-50
-    
+    <section id="header"  className={` fixed top-0 left-0 w-full  bg-gradient-to-r from-gray-200 to-gray-700 text-black  shadow-lg z-50
+    ${menuOpen && " rounded-b-xl"}
     `}>
       <div className="flex items-center justify-between h-20 px-4 md:px-10">
 
-        <div className="flex items-center gap-4 md:gap-2">
+        <div className="flex items-center gap-4 md:gap-3">
           <img
             src={images}
             alt="logo"
             className="w-12 h-12 rounded-full shadow-xl object-cover"
           />
-          <h1 className="text-lg md:text-lg lg:text-xl font-bold">PORTFOLIO</h1>
+          <h1 className="text-lg md:text-lg lg:text-xl font-serif">PORTFOLIO</h1>
         </div>
 
       
@@ -57,30 +58,38 @@ const Header = () => {
         </div>
 
         <div className={` md:hidden text-2xl transform transition-transform duration-300 ${menuOpen ? "-rotate-90" : "rotate-0"}`}  onClick={toggleMenu}>
-          {menuOpen ? <GiCrossedBones/> : <HiOutlineBars2 />}
+          {menuOpen ? <GiCrossedBones/> : <HiOutlineBars2 className="text-3xl"/>}
         </div>
       </div>
 
      {/* mobile view  */}
+      <AnimatePresence>
           {menuOpen && (
-        <div id="menu" 
-        className={`md:hidden flex flex-col border-black border-t-2 justify-center items-center 
-        `}>
-          {navItems.map((item, i) => (
-           <Link
-           key={i}
-           to={item.link}
-           smooth={true} 
-           duration={500}
-           offset={true}
-           spy={true}
-           activeClass="active-link"
-           className="text-lg cursor-pointer w-20  flex flex-col justify-center items-center pb-2 transition-all duration-300 "
-           onClick={() => setMenuOpen(false) }>{item.name}
-           </Link>
+        <motion.div id="menu" 
+         initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -50 }}
+            transition={{ duration: 0.3 }}
+        className={`md:hidden bg-inherit flex flex-col transform duration-150 rounded-b-full border-black border-t-2 justify-center items-center p-3 `}>
+         
+         <div className=" border-2 border-gray-700/80 px-4 py-2 rounded-lg">
+           {navItems.map((item, i) => (
+          <Link  
+          key={i}
+            to={item.link}
+            smooth={true} 
+            duration={500}
+            offset={true}
+            spy={true}
+            activeClass="active-link"
+            className="text-lg cursor-pointer p-0.5 flex flex-col justify-center items-center pb-2 px-28 rounded-lg transition-all duration-300 "
+            onClick={() => setMenuOpen(false) }>{item.name}
+          </Link>
           ))}
-        </div>
+         </div>
+        </motion.div>
       )}
+      </AnimatePresence>
     </section>
 
     
